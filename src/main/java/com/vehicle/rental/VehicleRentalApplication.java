31d841b684173834a8cf5f718209730e0dbd91ad
AshishGoy0 <ashgoy0@gmail.com>
2022-10-03 17:53:46 +0530

    remove extra spaces

--- src/main/java/com/vehicle/rental/VehicleRentalApplication.java
+++ src/main/java/com/vehicle/rental/VehicleRentalApplication.java
@@ -69,6 +69,7 @@ public class VehicleRentalApplication {
                 vehicles.toString()
                         .replace("[", "")
                         .replace("]", "")
+                        .replace(" ", "")
                         .trim()
         );
     }
